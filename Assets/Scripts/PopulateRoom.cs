2634849dbc8abe150b6b61ee1d2f5aa6ce68d8d1
MeatJun <meatjun@berkeley.edu>
2024-02-24 02:01:50 -0800

    Finished room population system

--- Assets/Scripts/PopulateRoom.cs
+++ Assets/Scripts/PopulateRoom.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class PopulateRoom : MonoBehaviour
 {
-    public GameObject[] furnitureSpawnLocations;
-    public GameObject[] possibleFurniture;
+    private static Transform possibleFurnitureLocations;
+    public GameObject[] possibleFurnitureTypes;
+    private static List<GameObject> furnitureChildren;
+    public int minimumNumberOfFurniture = 2;
+    public int maximumNumberOfFurniture = 4;
 
     void Start()
     {
-        
+        possibleFurnitureLocations = transform.Find("Furniture");
+        furnitureChildren = new List<GameObject>();
+
+        for (int i = 0; i < possibleFurnitureLocations.childCount; i++)
+        {
+            furnitureChildren.Add(possibleFurnitureLocations.GetChild(i).gameObject);
+        }
+
+        int currentNumberOfFurniture = 0;
+        minimumNumberOfFurniture = Random.Range(minimumNumberOfFurniture, maximumNumberOfFurniture);
+
+        while (currentNumberOfFurniture < minimumNumberOfFurniture) {
+            int locationIndex = Random.Range(0, furnitureChildren.Count);
+            int furnitureIndex = Random.Range(0, possibleFurnitureTypes.Length);
+            
+            GameObject furniture = furnitureChildren[locationIndex];
+            Instantiate(possibleFurnitureTypes[furnitureIndex], new Vector3(furniture.transform.position.x, furniture.transform.position.y, -1), Quaternion.identity);
+
+            furnitureChildren.RemoveAt(locationIndex);
+            currentNumberOfFurniture++;
+        }
     }
 }
