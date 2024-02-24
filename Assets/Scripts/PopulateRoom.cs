40ec0e2b9b6d0a4e62851c7ce0bc5d35c7b7dc4d
MeatJun <meatjun@berkeley.edu>
2024-02-24 01:27:27 -0800

    Added player and small room prefab

---
+++ Assets/Scripts/PopulateRoom.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateRoom : MonoBehaviour
+{
+    public GameObject[] furnitureSpawnLocations;
+    public GameObject[] possibleFurniture;
+
+    void Start()
+    {
+        
+    }
+}
